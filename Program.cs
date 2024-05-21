27cb277b0e67df488615a6823421ac5cc266d459
NintenHero <37460517+MichaelHinrichs@users.noreply.github.com>
2024-05-21 10:07:43 -0500

    Add Jack N' Jill DX to "written for" comment

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿//Written for ﻿games in the Spiller Engine, by Ratalaika Games. https://ratalaikagames.com/
-//The Minotaur https://store.steampowered.com/app/412540/
-//Defend Your Crypt https://store.steampowered.com/app/457450/
-//League of Evil https://store.steampowered.com/app/491060/
-//Squareboy vs Bullies Arena Edition https://store.steampowered.com/app/709620/
+﻿//Written for ﻿games in the Spiller Engine, by Ratalaika Games. https://ratalaikagames.com
+//The Minotaur https://store.steampowered.com/app/412540
+//Defend Your Crypt https://store.steampowered.com/app/457450
+//League of Evil https://store.steampowered.com/app/491060
+//Squareboy vs Bullies Arena Edition https://store.steampowered.com/app/709620
+//Jack N' Jill DX https://store.steampowered.com/app/873560
 using System;
 using System.IO;
 using System.IO.Compression;
